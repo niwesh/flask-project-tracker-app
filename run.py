67531a194688c8ec20cbbc3cc240179635379173
Nivesh Mishra <44528117+niwesh@users.noreply.github.com>
2023-11-03 22:58:35 +0530

    Fixed Port issue 1

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
+import os
+
 from app import create_app, db  # from the app package __init__
 
-# if __name__ == '__main__':
-#     flask_app = create_app('prod')
-#     with flask_app.app_context():
-#         db.create_all()
-#     flask_app.run()
+if __name__ == '__main__':
+    flask_app = create_app('prod')
+    with flask_app.app_context():
+        db.create_all()
+    flask_app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
 
-flask_app = create_app('prod')
-with flask_app.app_context():
-    db.create_all()
-    flask_app.run()
+# flask_app = create_app('prod')
+# with flask_app.app_context():
+#     db.create_all()
+#     flask_app.run()
