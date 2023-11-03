9662b40c9c8418ed90f525dbaf11c677039e09c2
Nivesh Mishra <44528117+niwesh@users.noreply.github.com>
2023-11-03 23:35:46 +0530

    Fixed port issue 6

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app import create_app, db
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
-    flask_app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    flask_app.run()
