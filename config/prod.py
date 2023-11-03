1d7b02cd20ac1159432a2336aa32fd25d34fb0db
Nivesh Mishra <44528117+niwesh@users.noreply.github.com>
2023-11-04 01:06:37 +0530

    Downgraded Falsk

--- config/prod.py
+++ config/prod.py
@@ -2,9 +2,10 @@ import os
 
 DEBUG = False
 SECRET_KEY = 'topsecret'
-# USERNAME = 'postgres' PASSWORD = 'Hello' DB_NAME = 'project_expense_tracker_db' SQLALCHEMY_DATABASE_URI =
-# f'postgresql://{USERNAME}:{PASSWORD}@localhost/{DB_NAME}' SQLALCHEMY_DATABASE_URI =
-# 'postgresql://nivesh_pg_user:Qqco25PDORSeKqUgSLviTnasTHNIaYU6@dpg-cl2k9c0310os73dhck5g-a.oregon-postgres.render.com
-# /nivesh_pg'
+# USERNAME = 'postgres'
+# PASSWORD = 'Hello'
+# DB_NAME = 'project_expense_tracker_db'
+# SQLALCHEMY_DATABASE_URI = f'postgresql://{USERNAME}:{PASSWORD}@localhost/{DB_NAME}'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://nivesh_pg_user:Qqco25PDORSeKqUgSLviTnasTHNIaYU6@dpg-cl2k9c0310os73dhck5g-a.oregon-postgres.render.com/nivesh_pg'
 SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
